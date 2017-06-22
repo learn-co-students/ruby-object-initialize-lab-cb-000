d6b6a9c839abd748cd08f660ae2cde21a61cc4e7
Josef Butts <jbutts@utexas.edu>
2017-06-22 13:16:44 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,26 @@
+class Person
+
+    def initialize(name)
+      @name = name
+    end
+
+
+    def name=(persons_name)
+      @name = persons_name
+    end
+
+    def name
+      @name
+    end
+
+    def job=(persons_job)
+      @job = persons_job
+    end
+
+    def job
+      @job
+    end
+
+
+
+end
